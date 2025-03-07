c41e0169a2af46327446b24e976824a23ef29759
Luke Brace <info.lukebracee@gmail.com>
2025-03-07 14:37:52 +0000

    Rock

---
+++ RockPaperScissors.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class RockPaperScissors
+{
+    private static final String ROCK_PAPER = """
+                _______
+           ---'     ____)
+                    (_____)
+                    (_____)
+                    (____)
+           ---.__(___)
+            """;
+}
